6182441544b01c2e68bb397a67f8855d59654ea2
syoutaoru.nozu <nkc.syoutarou.nozu@gmail.com>
2018-04-19 16:40:47 +0900

    NozuSceneにカメラの挙動を追加。仮画像も多少追加。

---
+++ Assets/Resources/Scripts/PhoneCamera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PhoneCamera : MonoBehaviour
+{
+
+    public Camera cam;
+    public RenderTexture RenderTextureRef;
+
+   
+    public static List<Texture2D> texs;
+
+    // Use this for initialization
+    void Start()
+    {
+        texs = new List<Texture2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("ScreenShot");
+            screenShot();
+        }
+
+    }
+
+    public void screenShot()
+    {
+        Texture2D tex = new Texture2D(RenderTextureRef.width, RenderTextureRef.height, TextureFormat.RGB24, false);
+        RenderTexture.active = RenderTextureRef;
+        tex.ReadPixels(new Rect(0, 0, RenderTextureRef.width, RenderTextureRef.height), 0, 0);
+        tex.Apply();
+
+
+        texs.Add(tex);
+
+
+        //// Encode texture into PNG
+        //byte[] bytes = tex.EncodeToPNG();
+        //Object.Destroy(tex);
+
+        ////Write to a file in the project folder
+        ////File.WriteAllBytes(Application.dataPath + "/../SavedScreen.png", bytes);
+        //File.WriteAllBytes("C:/Users/student/Desktop/SavedScreen.png", bytes);
+    }
+    public static List<Texture2D> GetTextures()
+    {
+        return texs;
+    }
+}
