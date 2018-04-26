19ca5b7cab6bf5e632961fbb21bdee6bbf24c61f
shota kasugai <ast@over-sky.net>
2018-04-26 12:07:45 +0900

    全体のシーケンスを一通り通した

---
+++ Assets/Resources/Scripts/GameMenuPush.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuPush : MonoBehaviour {
+
+    public static bool menu_active;
+
+	// Use this for initialization
+	void Start () {
+        menu_active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!menu_active && Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MenuScene", LoadSceneMode.Additive);
+            menu_active = true;
+        }
+	}
+
+    public static void setMenuActive(bool b)
+    {
+        menu_active = b;
+    }
+}
--- Assets/Resources/Scripts/MenuScript.cs
+++ Assets/Resources/Scripts/MenuScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
 
@@ -11,6 +12,20 @@ public class MenuScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Escape))//ゲームに戻る
+        {
+            SceneManager.UnloadSceneAsync("MenuScene");
+            GameMenuPush.setMenuActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))//ゲームに戻る
+        {
+            SceneManager.UnloadSceneAsync("MenuScene");
+            GameMenuPush.setMenuActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))//タイトルに戻る
+        {
+            SceneManager.LoadSceneAsync("TitleScene");
+            GameMenuPush.setMenuActive(false);
+        }
+    }
 }
