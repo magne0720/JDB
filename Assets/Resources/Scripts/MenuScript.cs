b3a96b502bf922acb5d7e0ce4c47d4854605075d
shota kasugai <ast@over-sky.net>
2018-05-10 12:41:47 +0900

    menuシーン変更

--- Assets/Resources/Scripts/MenuScript.cs
+++ Assets/Resources/Scripts/MenuScript.cs
@@ -2,30 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour {
 
+    public Image ImgTitle, ImgResume;
+    public Sprite TexTitle, TexTitleSub, TexResume, TexResumeSub;
+
+    public bool select = false;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        ImgTitle = ImgTitle.GetComponent<Image>();
+        ImgResume = ImgResume.GetComponent<Image>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Escape))//ゲームに戻る
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            SceneManager.UnloadSceneAsync("MenuScene");
-            GameMenuPush.setMenuActive(false);
+            select = !select;
         }
-        if (Input.GetKeyDown(KeyCode.Alpha1))//ゲームに戻る
+
+        if (select)
         {
-            SceneManager.UnloadSceneAsync("MenuScene");
-            GameMenuPush.setMenuActive(false);
+            ImgTitle.sprite = TexTitleSub;
+            ImgResume.sprite = TexResume;
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2))//タイトルに戻る
+        else
         {
-            SceneManager.LoadSceneAsync("TitleScene");
-            GameMenuPush.setMenuActive(false);
+            ImgTitle.sprite = TexTitle;
+            ImgResume.sprite = TexResumeSub;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (select)
+            {
+                SceneManager.LoadSceneAsync("TitleScene");
+                GameMenuPush.setMenuActive(false);
+            }
+            else
+            {
+                SceneManager.UnloadSceneAsync("MenuScene");
+                GameMenuPush.setMenuActive(false);
+            }
         }
     }
 }
--- Assets/Resources/Scripts/TestRote.cs
+++ Assets/Resources/Scripts/TestRote.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class TestRote : MonoBehaviour {
 
-    public GameObject obj;
-
 	// Use this for initialization
 	void Start () {
 		
@@ -13,21 +11,6 @@ public class TestRote : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.W)) obj.transform.position += obj.transform.forward * 0.1f;
-        if (Input.GetKey(KeyCode.S)) obj.transform.position -= obj.transform.forward * 0.1f;
-        if (Input.GetKey(KeyCode.D)) obj.transform.Rotate(0,0,-1);
-        if (Input.GetKey(KeyCode.A)) obj.transform.Rotate(0,0, 1);
-
-        //--//
-
-        Vector3 m = Vector3.zero;
-
-        m.y = Input.GetAxis("Mouse X");
-        m.x = Input.GetAxis("Mouse Y") * -1;
-
-        //obj.transform.Rotate(obj.transform.right, m.x);
-        //obj.transform.Rotate(obj.transform.up, m.y);
-
-        obj.transform.eulerAngles += m;
+        this.transform.Rotate(0, 1, 0);
     }
 }
