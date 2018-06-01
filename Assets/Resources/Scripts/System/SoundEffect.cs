d6258ac449f230d78a3734c0484dd86130ba367a
Ryo_Ohnishi <nkc.ryo.ohnishi@gmail.com>
2018-06-01 14:03:02 +0900

    歩行音、ダッシュ音切り替え

---
+++ Assets/Resources/Scripts/System/SoundEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour {
+
+    [Header("SoundEffect")]
+    [Space(12)]
+
+    [Tooltip("歩く音")]
+    public AudioClip Walk;
+    [Space(8)]
+
+    [Tooltip("走る音")]
+    public AudioClip Dash;
+    [Space(8)]
+
+    [Tooltip("扉の開く音")]
+    public AudioClip OpenDoor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Resources/Scripts/test/walk.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk : MonoBehaviour {
+    AudioSource audio;
+    SoundEffect SE;
+    int WalkState = 0;
+    bool walking;
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        SE = GameObject.Find("SEtest").GetComponent<SoundEffect>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                WalkState = 2;
+            }
+            else  WalkState = 1;
+        }
+        else { WalkState = 0; }
+
+        Debug.Log(WalkState);
+        OnSound();
+        if (!audio.isPlaying)
+        {
+            audio.Play();
+        }
+    }
+
+    void OnSound()
+    {
+        switch(WalkState)
+        {
+            case 0:audio.clip = null; break;
+            case 1:if(audio.clip != SE.Walk) audio.clip = SE.Walk;
+                break;
+            case 2:if(audio.clip != SE.Dash)audio.clip = SE.Dash;
+                break;
+
+            default: break;
+        }
+    }
+}
