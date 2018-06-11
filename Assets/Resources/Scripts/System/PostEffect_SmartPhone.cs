4c457d8f71b8fdfec4c4e935312e2f1f9d6759ae
shota kasugai <ast@over-sky.net>
2018-06-11 13:55:17 +0900

    IsoNoise

---
+++ Assets/Resources/Scripts/System/PostEffect_SmartPhone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffect_SmartPhone : MonoBehaviour
+{
+
+    public Material IsoNoise;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, IsoNoise);
+        IsoNoise.SetFloat("_T", Time.time);
+    }
+}
