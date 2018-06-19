f6b10a2ae8ab1969788d211a2235900698a8edc5
Ryo_Ohnishi <nkc.ryo.ohnishi@gmail.com>
2018-06-19 15:14:49 +0900

    メニューとゲームオーバー画面のポップアップ

---
+++ Assets/Resources/Scripts/test/PopUpScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PopUpScript : MonoBehaviour {
+
+
+    bool PopUpState;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (!PopUpState)
+            {
+                Application.LoadLevelAdditive("MenuScene");
+                PopUpState = true;
+            }
+            else
+            {
+                Application.UnloadLevel("MenuScene");
+                PopUpState = false;
+                Resources.UnloadUnusedAssets();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (!PopUpState)
+            {
+                Application.LoadLevelAdditive("GameOverScene");
+                PopUpState = true;
+            }
+            else
+            {
+                Application.UnloadLevel("GameOverScene");
+                PopUpState = false;
+                Resources.UnloadUnusedAssets();
+            }
+
+        }
+
+    }
+}
