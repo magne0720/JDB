24138077a705fa2ee42a54dbd30a773dd38637c9
Ryo_Ohnishi <nkc.ryo.ohnishi@gmail.com>
2018-06-22 10:31:25 +0900

    Merge branch 'master' of https://github.com/magne0720/JDB into 回避
    # Conflicts:
    #	.vs/JDB/v15/Server/sqlite3/storage.ide

---
+++ Assets/Resources/Scripts/System/SpawnRoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRoot : MonoBehaviour {
+
+    public List<Vector3> roots; 
+
+	// Use this for initialization
+	void Start () {
+        roots = new List<Vector3>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject g = transform.GetChild(i).gameObject;
+            roots.Add(g);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public List<Vector3> GetRoot()
+    {
+        return roots;
+    }
+}
